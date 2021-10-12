9388e5d930fb6da7098ded33461df66e1dc6b79e
ChrisWellsWood <cwwoodesq@gmail.com>
2021-10-12 23:47:16 +0100

    Feat: Adds a header.

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,11 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Font as Font
+import Html exposing (Html)
+
 
 
 ---- MODEL ----
@@ -36,10 +39,33 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    layout [] <|
+        portraitView model
+
+
+portraitView : Model -> Element Msg
+portraitView model =
+    column
+        [ width fill ]
+        [ title
+
+        --, timeline
+        --, details
         ]
+
+
+title : Element msg
+title =
+    el
+        [ width fill
+        , height <| fillPortion 1
+        , padding 60
+        , Background.color <| rgb255 143 192 169
+        , headerFont
+        , Font.size 40
+        ]
+    <|
+        text "The Designed Protein Archive"
 
 
 
@@ -54,3 +80,23 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+
+---- STYLE ----
+
+
+headerFont : Attribute msg
+headerFont =
+    Font.family
+        [ Font.typeface "Barlow"
+        , Font.sansSerif
+        ]
+
+
+bodyFont : Attribute msg
+bodyFont =
+    Font.family
+        [ Font.typeface "Lato"
+        , Font.sansSerif
+        ]
