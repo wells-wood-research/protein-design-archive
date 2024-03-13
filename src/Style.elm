d20485c441b24095d40b17b91a9cdbe4fbda4c49
Marta Chronowska <mchrnwsk@gmail.com>
2024-03-13 16:41:14 +0000

    Tidy up - shift functions related to Date and used by Timeline to DesignDate.elm ; shift functions related to Style to Style.elm ; add type annotation to defaultKeys in DesignFilter.elm

---
+++ src/Style.elm
@@ -0,0 +1,65 @@
+module Style exposing (..)
+
+import Element exposing (..)
+import Element.Font as Font
+
+
+
+---- STYLE ----
+-- https://coolors.co/faf3dd-c8d5b9-8fc0a9-68b0ab-696d7d
+-- eggshell 250 243 221
+-- tea green 200 213 185
+-- cambridge blue 143 192 169
+-- verdigris 104 176 171
+-- payne's gray 105 109 125
+
+
+titleFont : List (Attribute msg)
+titleFont =
+    [ Font.family
+        [ Font.typeface "Barlow"
+        , Font.sansSerif
+        ]
+    , Font.size 40
+    ]
+
+
+h1Font : List (Attribute msg)
+h1Font =
+    [ Font.family
+        [ Font.typeface "Lato"
+        , Font.sansSerif
+        ]
+    , Font.size 32
+    ]
+
+
+h2Font : List (Attribute msg)
+h2Font =
+    [ Font.family
+        [ Font.typeface "Lato"
+        , Font.sansSerif
+        ]
+    , Font.size 24
+    ]
+
+
+bodyFont : List (Attribute msg)
+bodyFont =
+    [ Font.family
+        [ Font.typeface "Lato"
+        , Font.sansSerif
+        ]
+    , Font.size 16
+    , Font.alignLeft
+    ]
+
+
+monospacedFont : List (Attribute msg)
+monospacedFont =
+    [ Font.family
+        [ Font.typeface "Fira Code"
+        , Font.sansSerif
+        ]
+    , Font.size 16
+    ]
