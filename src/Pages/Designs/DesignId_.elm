38f99369a174e758addffd6aec3d095b92b1fd32
Chris Wells Wood <cwwoodesq@gmail.com>
2024-03-14 15:09:34 +0000

    Changes the details page to a sandbox.

--- src/Pages/Designs/DesignId_.elm
+++ src/Pages/Designs/DesignId_.elm
@@ -1,9 +1,51 @@
-module Pages.Designs.DesignId_ exposing (page)
+module Pages.Designs.DesignId_ exposing (Model, Msg, page)
 
 import Html exposing (Html)
+import Page exposing (Page)
 import View exposing (View)
 
 
-page : { designId : String } -> View msg
-page params =
-    View.fromString <| "Pages.Designs.DesignId_" ++ params.designId
+page : { designId : String } -> Page Model Msg
+page { designId } =
+    Page.sandbox
+        { init = init designId
+        , update = update
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    { designId : String }
+
+
+init : String -> Model
+init designId =
+    { designId = designId }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    View.fromString <| "Pages.Designs.DesignId_" ++ model.designId
