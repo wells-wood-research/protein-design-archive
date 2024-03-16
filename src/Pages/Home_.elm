f6c59067dcdffbafc679d2853b7b000501df4a97
Chris Wells Wood <cwwoodesq@gmail.com>
2024-03-16 12:18:19 +0000

    Changes home page to fully fledged page.

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,12 +1,71 @@
-module Pages.Home_ exposing (page)
+module Pages.Home_ exposing (Model, Msg, page)
 
+import Effect exposing (Effect)
 import Element exposing (..)
+import Page exposing (Page)
+import Route exposing (Route)
+import Shared
 import View exposing (View)
 
 
-page : View msg
-page =
-    { title = "Homepage"
+page : Shared.Model -> Route () -> Page Model Msg
+page _ _ =
+    Page.new
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Effect Msg )
+init () =
+    ( {}
+    , Effect.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model
+            , Effect.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view _ =
+    { title = "Protein Design Archive"
     , attributes = []
-    , element = text "Hello world!"
+    , element = text "Home"
     }
