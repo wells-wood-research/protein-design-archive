fc9bae5b48bd31f8128aec28cfa219b230b1f954
ChrisWellsWood <cwwoodesq@gmail.com>
2024-04-16 12:36:05 +0100

    Adds an api endpoint for getting design stubs.

--- backend/src/backend/__init__.py
+++ backend/src/backend/__init__.py
@@ -3,5 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def serve_application():
     return "<p><b>Hello</b>, World!</p>"
+
+@app.get("/all-design-stubs")
+def get_all_design_stubs():
+    """Gets all the design stub data for the front page."""
+    # query the database using pymongo
+    return {"design1": "blah"}
