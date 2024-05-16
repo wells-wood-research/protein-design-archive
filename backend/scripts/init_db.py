84ca3c697969aa2b8e8be308380cab10ac13c596
ChrisWellsWood <cwwoodesq@gmail.com>
2024-05-16 14:04:35 +0100

    Updates init file for database.

--- backend/scripts/init_db.py
+++ backend/scripts/init_db.py
@@ -1,20 +1,30 @@
-import argparse
 import json
+import typing as t
+
+import click
+
+from backend.db import DESIGNS
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog="init_db",
-        description="Initialises PDA database.",
-    )
-    parser.add_argument("filename", help="Path to raw design data from database.")
-    args = parser.parse_args()
-
-    with open(args.filename, "r") as inf:
-        print(args.filename)
-        # designs = json.load(inf)
-        # print(designs)
+@click.command()
+@click.argument("input_json_path", type=click.Path(exists=True))
+def main(input_json_path):
+    """CLI tool for import the database."""
+    click.echo(f"Loading input file: {input_json_path}")
+    design_records = import_from_json(input_json_path)
+    write_records_to_db(design_records)
 
 
-def import_from_json():
-    return
+def import_from_json(json_path) -> t.List[t.Dict[str, t.Any]]:
+    with open(json_path) as json_file:
+        design_records = json.load(json_file)
+    return design_records
+
+
+def write_records_to_db(design_records):
+    returned = DESIGNS.insert_many(design_records)
+    click.echo(returned)
+
+
+if __name__ == "__main__":
+    main()
