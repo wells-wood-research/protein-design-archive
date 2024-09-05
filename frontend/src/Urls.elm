b15b056654f70dc67e768d41accd33ebd94e5864
Marta Chronowska <mchrnwsk@gmail.com>
2024-09-05 13:55:17 +0100

    Change endpoints for download from localhost 5000 to https://pragmaticproteindesign.bio.ed.ac.uk/pda-api/ url

--- frontend/src/Urls.elm
+++ frontend/src/Urls.elm
@@ -6,14 +6,14 @@ import Json.Encode as Encode
 downloadSelectedDesigns : List String -> String
 downloadSelectedDesigns pdbCodes =
     if List.isEmpty pdbCodes then
-        "http://localhost:5000/all-designs"
+        "https://pragmaticproteindesign.bio.ed.ac.uk/pda-api/all-designs"
 
     else
         let
             encodedPdbCodes =
                 Encode.encode 0 (Encode.list Encode.string pdbCodes)
         in
-        "http://localhost:5000/all-designs?pdb-codes=" ++ encodedPdbCodes
+        "https://pragmaticproteindesign.bio.ed.ac.uk/pda-api/all-designs?pdb-codes=" ++ encodedPdbCodes
 
 
 allDesignStubs : String
