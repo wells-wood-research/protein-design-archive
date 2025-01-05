64ef62fa1fa00f30ad5947cda0a60da6bf5853dd
Marta Chronowska <mchrnwsk@gmail.com>
2025-01-05 19:31:51 +0000

    Update date of last update

--- frontend/src/Components/Title.elm
+++ frontend/src/Components/Title.elm
@@ -97,5 +97,5 @@ footerArea =
                         ]
                 }
             ]
-        , row [ width <| fillPortion 1, centerX, padding 10 ] [ text <| "Date of last update: " ++ "2024-10-31" ]
+        , row [ width <| fillPortion 1, centerX, padding 10 ] [ text <| "Date of last update: " ++ "2025-01-01" ]
         ]
