5a15c21aad855b2e8883cb2849e576cf2eb46bf6
Marta Chronowska <mchrnwsk@gmail.com>
2025-02-04 13:18:34 +0000

    Feb25 update of the dataset

--- frontend/src/Components/Title.elm
+++ frontend/src/Components/Title.elm
@@ -97,5 +97,5 @@ footerArea =
                         ]
                 }
             ]
-        , row [ width <| fillPortion 1, centerX, padding 10 ] [ text <| "Date of last update: " ++ "2025-01-01" ]
+        , row [ width <| fillPortion 1, centerX, padding 10 ] [ text <| "Date of last update: " ++ "2025-02-01" ]
         ]
