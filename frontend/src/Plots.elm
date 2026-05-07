5ed42ec8650590bb8c6ce6092ebb5eb8a8696717
Marta Chronowska <mchrnwsk@gmail.com>
2026-05-07 21:22:03 +0100

    Improve look of timeline plot: less aggressive gravity, more centred

--- frontend/src/Plots.elm
+++ frontend/src/Plots.elm
@@ -1,12 +1,12 @@
-module Plots exposing (PlotData, RenderPlotState(..), timelinePlotDesigns, timelinePlotStubs, timelinePlotView)
+module Plots exposing (PlotData, RenderPlotState(..), timelinePlotStubs, timelinePlotView)
 
 import Date exposing (Unit(..))
 import Element exposing (..)
 import Element.Keyed as Keyed
-import Get exposing (getScreenWidthInt, getScreenWidthString)
+import Get exposing (getScreenWidthString)
 import Html
 import Html.Attributes as HAtt
-import ProteinDesign exposing (ProteinDesign, ProteinDesignStub)
+import ProteinDesign exposing (ProteinDesignStub)
 import Time exposing (Month(..))
 import Urls
 import Vega exposing (..)
@@ -31,30 +31,29 @@ timelinePlotId =
     "timeline-plot"
 
 
-timelinePlotView : Maybe Float -> Element msg
-timelinePlotView widthF =
+timelinePlotView : Float -> Float -> Element msg
+timelinePlotView widthF heightF =
     let
-        widthL =
-            px (getScreenWidthInt widthF)
-
         widthS =
-            getScreenWidthString widthF
+            getScreenWidthString (Just widthF)
     in
     column
-        [ spacing 15, Element.width widthL, centerX ]
+        [ spacing 15
+        , Element.width (px (round widthF))
+        , centerX
+        , Element.height (px (round heightF))
+
+        -- This prevents the "bleeding" onto the download area
+        , htmlAttribute (HAtt.style "overflow" "hidden")
+        ]
         [ Keyed.el [ centerX ]
             ( timelinePlotId
             , Html.div
                 [ HAtt.id timelinePlotId
                 , HAtt.style "width" widthS
+                , HAtt.style "height" (String.fromInt (round heightF) ++ "px")
                 ]
-                [ Html.div
-                    [ HAtt.style "border-radius" "5px"
-                    , HAtt.style "width" widthS
-                    , HAtt.style "background-color" "#d3d3d3"
-                    ]
-                    []
-                ]
+                []
                 |> html
             )
         ]
@@ -62,113 +61,6 @@ timelinePlotView widthF =
 
 
 ---- VEGA SPECS ----
-
-
-timelinePlotDesigns : Float -> List ProteinDesign -> PlotData
-timelinePlotDesigns widthF designs =
-    let
-        ds =
-            let
-                designsTable =
-                    dataFromColumns "designs" []
-                        << dataColumn "year"
-                            (List.map .release_date designs
-                                |> List.map Date.year
-                                |> List.map toFloat
-                                |> vNums
-                            )
-                        << dataColumn "month"
-                            (List.map .release_date designs
-                                |> List.map Date.monthNumber
-                                |> List.map toFloat
-                                |> vNums
-                            )
-                        << dataColumn "id"
-                            (List.map .pdb designs
-                                |> vStrs
-                            )
-                        << dataColumn "href"
-                            (List.map (\d -> Urls.internalRelatedLink ++ d.pdb) designs
-                                |> vStrs
-                            )
-            in
-            dataSource [ designsTable [] ]
-
-        si =
-            signals
-                << signal "cx" [ siUpdate "width / 2" ]
-                << signal "cy" [ siUpdate "height / 2 + 100" ]
-                << signal "radius" [ siValue (vNum 4) ]
-                << signal "collide" [ siValue (vNum 1) ]
-                << signal "gravityX" [ siValue (vNum 10) ]
-                << signal "gravityY" [ siValue (vNum 0.03) ]
-                << signal "static" [ siValue vTrue ]
-
-        sc =
-            scales
-                << Vega.scale "xScale"
-                    [ scType scLinear
-                    , scRange raWidth
-                    , scDomain (doData [ daDataset "designs", daField (field "year") ])
-                    , scZero false
-                    ]
-                << Vega.scale "cScale"
-                    [ scType scOrdinal
-                    , scRange (raScheme (str "category20c") [])
-                    ]
-
-        ax =
-            axes
-                << axis "xScale"
-                    siTop
-                    [ axDomain false
-                    , axFormat <| str ".4"
-                    ]
-
-        mk =
-            marks
-                << mark symbol
-                    [ mName "nodes"
-                    , mFrom [ srData (str "designs") ]
-                    , mEncode
-                        [ enEnter
-                            [ maFill [ vScale "cScale", vField (field "year") ]
-                            , maCustom "xFocus" [ vScale "xScale", vField (field "year"), vBand (num 0.5) ]
-                            , maCustom "yFocus" [ vSignal "cy" ]
-                            ]
-                        , enUpdate
-                            [ maSize [ vSignal "100" ]
-                            , maStroke [ white ]
-                            , maStrokeWidth [ vNum 1 ]
-                            , maZIndex [ vNum 0 ]
-                            ]
-                        , enHover
-                            [ maStroke [ vStr "purple" ]
-                            , maStrokeWidth [ vNum 3 ]
-                            , maZIndex [ vNum 1 ]
-                            , maCursor [ cursorValue cuPointer ]
-                            , maTooltip [ vField (field "id") ]
-                            , maHRef [ vField (field "href") ]
-                            ]
-                        ]
-                    , mTransform
-                        [ trForce
-                            [ fsIterations (num 50)
-                            , fsStatic (booSignal "static")
-                            , fsForces
-                                [ foCollide (numSignal "radius") [ fpIterations (numSignal "collide") ]
-                                , foX (field "xFocus") [ fpStrength (numSignal "gravityX") ]
-                                , foY (field "yFocus") [ fpStrength (numSignal "gravityY") ]
-                                ]
-                            ]
-                        ]
-                    ]
-    in
-    { plotId = timelinePlotId
-    , spec =
-        toVega
-            [ Vega.width (0.85 * widthF), Vega.height 300, ds, si [], sc [], ax [], mk [] ]
-    }
 
 
 timelinePlotStubs : Float -> Float -> List ProteinDesignStub -> PlotData
@@ -204,35 +96,25 @@ timelinePlotStubs widthF heightF designs =
         si =
             signals
                 << signal "cx" [ siUpdate "width / 2" ]
-                << signal "cy" [ siUpdate "height / 2 + 100" ]
-                << signal "radius" [ siValue (vNum 4) ]
+                << signal "cy" [ siUpdate "(height / 2) + 200" ]
+                << signal "radius" [ siValue (vNum 5) ]
                 << signal "collide" [ siValue (vNum 1) ]
-                << signal "gravityX" [ siValue (vNum 10) ]
-                << signal "gravityY" [ siValue (vNum 0.03) ]
+                << signal "gravityX" [ siValue (vNum 8) ]
+                << signal "gravityY" [ siValue (vNum 0.05) ]
                 << signal "static" [ siValue vTrue ]
-
-        sc =
-            scales
-                << Vega.scale "xScale"
-                    [ scType scLinear
-                    , scRange raWidth
-                    , scDomain (doData [ daDataset "designs", daField (field "year") ])
-                    , scZero false
-                    ]
-                << Vega.scale "cScale"
-                    [ scType scOrdinal
-                    , scRange (raScheme (str "category20c") [])
-                    ]
 
         ax =
             axes
                 << axis "xScale"
                     siTop
+                    -- Labels at the top
                     [ axDomain false
                     , axFormat <| str ".4"
                     , axTickCount <| num 16
-                    , axOffset <| vNum 250 -- vNum 80.0 # if siBottom
                     , axLabelFontSize <| num 12.0
+
+                    -- This ensures the ticks and labels have a small buffer from the top edge
+                    , axOffset (vNum 5)
                     ]
 
         mk =
@@ -247,10 +129,10 @@ timelinePlotStubs widthF heightF designs =
                             , maCustom "yFocus" [ vSignal "cy" ]
                             ]
                         , enUpdate
-                            [ maSize [ vSignal "100" ]
+                            -- Match visual size to collision radius (Area = pi * r^2)
+                            [ maSize [ vSignal "pow(radius, 2) * 3.14" ]
                             , maStroke [ white ]
-                            , maStrokeWidth [ vNum 1 ]
-                            , maZIndex [ vNum 0 ]
+                            , maStrokeWidth [ vNum 0.5 ]
                             ]
                         , enHover
                             [ maStroke [ vStr "purple" ]
@@ -263,24 +145,43 @@ timelinePlotStubs widthF heightF designs =
                         ]
                     , mTransform
                         [ trForce
-                            [ fsIterations (num 50)
+                            [ fsIterations (num 80) -- Reduce from 150. Too many iterations "crystallizes" the cloud.
                             , fsStatic (booSignal "static")
                             , fsForces
-                                [ foCollide (numSignal "radius") [ fpIterations (numSignal "collide") ]
+                                [ -- Reduce fpIterations to 1 or 2 to allow for a "looser" feel
+                                  foCollide (numSignal "radius") [ fpIterations (num 1) ]
                                 , foX (field "xFocus") [ fpStrength (numSignal "gravityX") ]
                                 , foY (field "yFocus") [ fpStrength (numSignal "gravityY") ]
                                 ]
                             ]
                         ]
                     ]
+
+        sc =
+            scales
+                << Vega.scale "xScale"
+                    [ scType scLinear
+                    , scRange raWidth
+                    , scDomain (doData [ daDataset "designs", daField (field "year") ])
+                    , scZero false
+                    ]
+                << Vega.scale "cScale"
+                    [ scType scOrdinal
+                    , scRange (raScheme (str "category20c") [])
+                    ]
     in
     { plotId = timelinePlotId
     , spec =
-        if widthF < 800.0 then
-            toVega
-                [ Vega.width (0.9 * widthF), Vega.height (0.5 * heightF), Vega.paddings 0 30 0 10, ds, si [], sc [], ax [], mk [] ]
+        toVega
+            [ Vega.width (0.85 * widthF)
+            , Vega.height (heightF - 100) -- Give more room for top/bottom margins
 
-        else
-            toVega
-                [ Vega.width (0.85 * widthF), Vega.height (0.5 * heightF), Vega.paddings 50 30 0 10, ds, si [], sc [], ax [], mk [] ]
+            -- Increase top padding specifically for the siTop axis labels
+            , Vega.paddings 40 20 40 20
+            , ds
+            , si []
+            , sc []
+            , ax []
+            , mk []
+            ]
     }
